d319f110a0a9a7dd79edda88ea3eeb9c24eadd15
Adam <amarinelli@esri.ca>
2016-01-09 16:50:19 -0500

    Chapter 6: Components
    Ludu: Discover Ember 2

---
+++ app/components/chirps-list/chirp-message/component.js
@@ -0,0 +1,7 @@
+// app/components/chirps-list/chirp-message/component.js
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'li'
+});
---
+++ app/components/chirps-list/component.js
@@ -0,0 +1,8 @@
+// app/components/chirps-list/component.js
+
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  tagName: 'ul',
+  classNames: ['card']
+});
