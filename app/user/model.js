d20c11c1dbd60821d068415630bd2aae80d27620
Adam <amarinelli@esri.ca>
2016-01-10 17:23:18 -0500

    Upgraded Ember to 2.2.0
    - started chapter 7, Ember Data, models

---
+++ app/user/model.js
@@ -0,0 +1,10 @@
+// app/user/model.js
+
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  username: DS.attr('string'),
+  numberOfChirps: DS.attr('number'),
+  numberOfFollowing: DS.attr('number'),
+  numberOfFollowers: DS.attr('number')
+});
